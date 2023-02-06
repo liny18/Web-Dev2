94775e252928b8d2587e5ec3eb3ce440385574ae
liny18 <liny18@rpi.edu>
2023-02-06 14:06:55 -0500

    setup-vite
    just found out about this cool thing about vite and im using it

--- lab3/server/server.js
+++ lab3/server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-app.get("/api", (req, res) => {
+app.get("/api/v1", (req, res) => {
   res.json({ users: ["user1", "user2", "user3", "user4"] });
 });
 
