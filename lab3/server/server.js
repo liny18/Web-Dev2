72abdb62d84b6a3d9c764b9664921b0378880759
liny18 <liny18@rpi.edu>
2023-02-06 11:00:35 -0500

    set up tailwind css

--- lab3/server/server.js
+++ lab3/server/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3001;
 
-app.get('/api', (req, res) => {
-  res.json({ "users": ["user1", "user2", "user3"] });
+app.get("/api", (req, res) => {
+  res.json({ users: ["user1", "user2", "user3", "user4"] });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
