dcec8dc4721de03025de0fffb794d963e39b27ae
Terry Lin <yuxlin21@gmail.com>
2023-02-05 22:20:43 -0500

    basic test
    testing frontend to backend interaction

--- lab3/client/src/App.tsx
+++ lab3/client/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState<any>([{}]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {typeof data.users === "undefined" ? (
+        <p>loading...</p>
+      ) : (
+        data.users.map((user: any, index: number) => {
+          return <p key={index}>{user}</p>;
+        })
+      )}
     </div>
   );
 }
