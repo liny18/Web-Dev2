1d5b8a03a50a125634218c7b200f60d81d6b6fe5
Terry Lin <yuxlin21@gmail.com>
2023-02-09 02:18:06 -0500

    implemented language change
    change language of card upon selection

--- lab3/client/src/components/Header.tsx
+++ lab3/client/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Dropdown } from "./Dropdown";
+import { DropdownMenu } from "./Dropdown";
 import { Searchbar } from "./Searchbar";
 
 
@@ -7,7 +7,7 @@ export const Header = () => {
     <div className="flex flex-row justify-between items-center border-b-2 border-b-gray-300 py-5">
       <h1 className="font-bold text-lg">Atlas</h1>
       <div className="flex flex-row items-center gap-5">
-        <Dropdown />
+        <DropdownMenu />
         <Searchbar />
       </div>
     </div>
