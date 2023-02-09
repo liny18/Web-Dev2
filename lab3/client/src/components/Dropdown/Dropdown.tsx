1c76245ca6ca8f435309211a6d8094d1f36f118b
liny18 <liny18@rpi.edu>
2023-02-09 08:12:30 -0500

    make code look less spaghetti
    run prettier on files

--- lab3/client/src/components/Dropdown/Dropdown.tsx
+++ lab3/client/src/components/Dropdown/Dropdown.tsx
@@ -1,46 +1,57 @@
 import React, { useContext, useState, useEffect } from "react";
-import 'semantic-ui-css/semantic.min.css'
-import { Dropdown } from 'semantic-ui-react'
-import { UserContext } from '../App'
-import styles from './Dropdown.module.css'
+import "semantic-ui-css/semantic.min.css";
+import { Dropdown } from "semantic-ui-react";
+import { UserContext } from "../../App";
+import styles from "./Dropdown.module.css";
 
 export const DropdownMenu = () => {
   const context = useContext(UserContext);
   const translations = context.allTranslations;
-  
-  const accessProperties = (str: string, translations: {[key: string]: {common: string, official: string}}[]) => {
+
+  const accessProperties = (
+    str: string,
+    translations: { [key: string]: { common: string; official: string } }[]
+  ) => {
     let result = { common: "", official: "" };
     translations.forEach((translation) => {
-        const key = Object.keys(translation)[0];
-        if (key === str) {
-            result = translation[key];
-        }
+      const key = Object.keys(translation)[0];
+      if (key === str) {
+        result = translation[key];
+      }
     });
-    console.log([result.common, result.official]);
     return [result.common, result.official];
-};
+  };
 
   useEffect(() => {
-    const [common, official] = accessProperties(context.selectedTranslation, translations);
+    const [common, official] = accessProperties(
+      context.selectedTranslation,
+      translations
+    );
     context.setCommon(common);
     context.setOfficial(official);
   }, [context.selectedTranslation]);
 
   const handleChange = (e: any, { value }: any) => {
     context.setSelectedTranslation(value);
-  }
-  
+  };
+
   const friendOptions = translations.map((translation) => {
     const key = Object.keys(translation)[0];
     return {
       key: key,
       text: key.toUpperCase(),
       value: key,
-    }
-  })
+    };
+  });
 
   return (
-  <Dropdown className={styles.drop} placeholder={context.selectedTranslation} fluid selection options={friendOptions} onChange={handleChange} />
-
-  )
-}
+    <Dropdown
+      className={styles.drop}
+      placeholder={context.selectedTranslation}
+      fluid
+      selection
+      options={friendOptions}
+      onChange={handleChange}
+    />
+  );
+};
--- lab3/client/src/components/Info.tsx
+++ lab3/client/src/components/Info.tsx
@@ -8,15 +8,23 @@ export const Info = () => {
     <div className="flex flex-col gap-2 bg-white bg-opacity-20 border-l-teal-100 border-l-4 p-5 fixed top-[40%] shadow-md">
       <p className="font-medium text-2xl text-white">{context.choice}</p>
       {context.choice === "Map" ? (
-        <a className="flex flex-row items-center gap-2 font-bold text-white overflow-y-scroll max-w-5xl max-h-24" href={context.map} target="_blank" rel="noreferrer">
+        <a
+          className="flex flex-row items-center gap-2 font-bold text-white overflow-y-scroll max-w-5xl max-h-24"
+          href={context.map}
+          target="_blank"
+          rel="noreferrer"
+        >
           <p className="font-semibold text-6xl"> View it on GoogleMaps</p>
-          <SiOpenstreetmap className="font-semibold text-6xl"/>
+          <SiOpenstreetmap className="font-semibold text-6xl" />
         </a>
       ) : (
-        <p className="font-bold text-8xl text-white overflow-y-scroll max-w-5xl max-h-24">"{context.output}"</p>
-      )
-      }
-      <p className="font-semibold text-lg text-white">Click on another button to change content.</p>
+        <p className="font-bold text-8xl text-white overflow-y-scroll max-w-5xl max-h-24">
+          "{context.output}"
+        </p>
+      )}
+      <p className="font-semibold text-lg text-white">
+        Click on another button to change content.
+      </p>
     </div>
   );
 };
