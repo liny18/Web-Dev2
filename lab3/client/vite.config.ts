8cca578f06173f521491aed3edb5329b85d9bc74
liny18 <liny18@rpi.edu>
2023-02-13 14:56:02 -0500

    Update vite.config.ts

--- lab3/client/vite.config.ts
+++ lab3/client/vite.config.ts
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://localhost:3001/",
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://liny18.eastus.cloudapp.azure.com/node/",
+      },
+    },
+  },
   plugins: [react()],
   base: './'
 })
