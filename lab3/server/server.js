feb466b44b43f9aa81b96624ddf7c2bc15aab3af
liny18 <liny18@rpi.edu>
2023-02-15 21:01:22 -0500

    include path

--- lab3/server/server.js
+++ lab3/server/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 const router = express.Router();
+const path = require("path");
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../client/dist")));
