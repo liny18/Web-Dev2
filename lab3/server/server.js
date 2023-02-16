2c4973fc1600b150312875338843add21eb8f36a
liny18 <liny18@rpi.edu>
2023-02-16 01:06:01 -0500

    Update server.js

--- lab3/server/server.js
+++ lab3/server/server.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const router = express.Router();
+const router = express.Router({strict: true});
 const path = require("path");
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../client/dist"), { redirect: false }));
+app.use(express.static(path.join(__dirname, "../client/dist"), { redirect: false, index: false }));
 
 router.get("/images", (req, res) => {
   const query = req.query.country;
