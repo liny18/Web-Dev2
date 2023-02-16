91f23c908aa88f1b583d90b53f982c27a42476a5
liny18 <liny18@rpi.edu>
2023-02-16 09:06:36 -0500

    Update server.js

--- lab3/server/server.js
+++ lab3/server/server.js
@@ -8,9 +8,9 @@ const path = require("path");
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+// });
 
 app.get("/api/v1/images", (req, res) => {
   const query = req.query.country;
