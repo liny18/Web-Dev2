e0d6e2e13ed5522d1e03e89d8449f2f4a9b0677b
liny18 <liny18@rpi.edu>
2023-02-16 18:19:19 -0500

    test

--- lab3/server/server.js
+++ lab3/server/server.js
@@ -7,7 +7,7 @@ const path = require("path");
 
 app.use(express.json());
 
-app.use(express.static('../client/dist'));
+app.use("/ITWS4500-S23-liny18", express.static(path.join(__dirname, "../client", "dist")));
 
 // app.get("/", (req, res) => {
 //   res.sendFile("index.html", { root: path.join(__dirname, "../client", "dist") });
