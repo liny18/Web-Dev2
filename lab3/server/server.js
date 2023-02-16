16a2dd6ccb8423a3304f7f7dab64a555756e2554
liny18 <liny18@rpi.edu>
2023-02-16 00:39:46 -0500

    Update server.js

--- lab3/server/server.js
+++ lab3/server/server.js
@@ -4,10 +4,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const router = express.Router();
-const path = require("path");
+// const path = require("path");
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..client/dist")));
+// app.use(express.static(path.join(__dirname, "..client/dist")));
 
 router.get("/images", (req, res) => {
   const query = req.query.country;
