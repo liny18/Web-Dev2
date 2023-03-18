a6c749f06662269f201a9cfe7296e92f5046fa8d
Terry Lin <yuxlin21@gmail.com>
2023-03-18 18:45:26 -0400

    set up lab5

---
+++ lab5/client/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import { DropdownMenu } from "./Dropdown/Dropdown";
+import { Searchbar } from "./Searchbar";
+
+export const Header = () => {
+  return (
+    <div className="flex flex-row justify-between items-center border-b-2 border-b-gray-300 py-5">
+      <h1 className="font-bold text-lg">Atlas</h1>
+      <div className="flex flex-row items-center gap-5">
+        <DropdownMenu />
+        <Searchbar />
+      </div>
+    </div>
+  );
+};
